bca48692a3f38b7109ac2889fcff3c30630f2fb1
PortugalBR <121282411+PortugalBR@users.noreply.github.com>
2023-01-04 10:58:59 -0300

    Update and rename teste 2.py to sorteio_simples.py

--- sorteio_simples.py
+++ sorteio_simples.py
@@ -2,12 +2,5 @@ import random
 
 pessoas = [ 'Wandson Ramos','Gabriel Cesar','Caio Rego' ]
 
-
 print('SORTEIO')
 print('O ganhador foi: ' + pessoas [ random.randrange ( len ( pessoas ))])
-
-
-
-
-#2 PAPEL , ganha de pedra e perde pra tesoura
-#3 tesoura, ganha de papel e perde para pedra
