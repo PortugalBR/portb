c25855f3cab2e4593b577493aedcb6cba56681c4
PortugalBR <121282411+PortugalBR@users.noreply.github.com>
2023-01-04 10:57:17 -0300

    Delete aula09.py

--- aula09.py
+++
@@ -1,6 +0,0 @@
-frase = 'curso me video python'
-print(frase.replace('python','android')
-      )
-
-frase ='pinto no cu cu no pinto, pinto no cu do pinto, cu no pinto do cu'
-print(frase.count('o'))
