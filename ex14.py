2c31ad242e248e51f05ad36e902f2617ceafc8b2
PortugalBR <121282411+PortugalBR@users.noreply.github.com>
2023-01-03 23:23:54 -0300

    Delete ex14.py

--- ex14.py
+++
@@ -1,7 +0,0 @@
-import pygame as pygame
-pygame.init()
-mixer.music.music.load('ex14.mp3')
-mixer.music.play()
-input('Agora você escuta?')
-pygame.mixer.music.play(loops=0, start=0.0)
-pygame.event.wait()
